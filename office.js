b20fd01cd730bd4785dbd8bb53e1900e0c27f64d
zacharyp32 <zacharyp32@gmail.com>
2020-01-04 10:32:06 -0800

    lots-changes

--- office.js
+++ office.js
@@ -17,16 +17,56 @@ function displayInputs(e) {
 
 function clearPaperWork() {
   const inputsWrapper = [...document.querySelectorAll(".real-inputs")];
-  inputsWrapper.forEach(item => item.value = "");
+  inputsWrapper.forEach(item => (item.value = ""));
 }
 
 // -------------------------------------------------------------------
 
 function paperWork() {
   //Grab HTML elements--------------------------------------
+  const genId = document.getElementById("general-id");
+  const furId = document.getElementById("fur-id");
+  const evapId = document.getElementById("evap-id");
+  const miniSplitId = document.getElementById("mini-split-id");
+  const acId = document.getElementById("ac-id");
+  const packUnitId = document.getElementById("pack-unit-id");
+  const heatPumpId = document.getElementById("heat-pump-id");
+  const ductId = document.getElementById("duct-id");
+  const ductExId = document.getElementById("duct-exempt-id");
+  const ductInfoId = document.getElementById("duct-info-id");
+  const rcaId = document.getElementById("rca-id");
+  const airflowId = document.getElementById("airflow-id");
+  const iaqId = document.getElementById("iaq-id");
+  const bdId = document.getElementById("bd-id");
+  const fwdId = document.getElementById("fwd-id");
+  const statusId = document.getElementById("status-id");
+  const qcId = document.getElementById("qc-id");
+  const notesId = document.getElementById("notes-id");
+
+  const genAnsWrapper = document.getElementById("gen-ans-wrapper");
+  const furAnsWrapper = document.getElementById("fur-ans-wrapper");
+  const evapAnsWrapper = document.getElementById("evap-ans-wrapper");
+  const acAnsWrapper = document.getElementById("ac-ans-wrapper");
+  const hPumpAnsWrapper = document.getElementById("hpump-ans-wrapper");
+  const packUnitAnsWrapper = document.getElementById("pac-unit-ans-wrapper");
+  const miniSplitAnsWrapper = document.getElementById("mini-split-ans-wrapper");
+  const leakAnsWrapper = document.getElementById("leak-ans-wrapper");
+  const ductExWrapper = document.getElementById("duct-exempt-wrapper");
+  const ductInfoWrapper = document.getElementById("duct-info-wrapper");
+  const rcaAnsWrapper = document.getElementById("rca-ans-wrapper");
+  const flowAnsWrapper = document.getElementById("flow-ans-wrapper");
+  const iaqAnsWrapper = document.getElementById("iaq-ans-wrapper");
+  const bdAnsWrapper = document.getElementById("bd-ans-wrapper");
+  const fwdAnsWrapper = document.getElementById("fwd-ans-wrapper");
+  const statusAnsWrapper = document.getElementById("stat-ans-wrapper");
+  const qcAnsWrapper = document.getElementById("qc-ans-wrapper");
+  const notesAnsWrapper = document.getElementById("notes-ans-wrapper");
+
   const dateInput = document.getElementById("date-input");
   const adrInput = document.getElementById("address-input");
   const companyInput = document.getElementById("company-input");
+  const rebateInput = document.getElementById("rebate-input");
+  const sqftInput = document.getElementById("sqft-input");
 
   const systemIdInput = document.getElementById("unit-num-input");
   const packUnitCheck = document.getElementById("punit-check-input");
@@ -73,15 +113,18 @@ function paperWork() {
   const prevTestedCheck = document.getElementById("already-tested");
   const asbestos = document.getElementById("a-b");
 
+  const dltMethodInput = document.getElementById("dlt-method-input");
   const dlTargetInput = document.getElementById("dlt-input");
+  const dltAllowInput = document.getElementById("allow-dlt-input");
   const leakage = document.getElementById("leakage");
   const openCheck = document.getElementById("open-check");
-  const pbsCheck = document.getElementById("pbs-check");
   const btwFloorsCheck = document.getElementById("btw-floors");
   const leakOutside = document.getElementById("leak-outside-check");
   const actualCheck = document.getElementById("actual-check");
+  const pbsCheck = document.getElementById("pbs-check");
+  const dltPass = document.getElementById("dlt-pass");
+  const dltFail = document.getElementById("dlt-fail");
 
-  const weighInInput = document.getElementById("weigh-in-input");
   const lpInput = document.getElementById("lp-input");
   const hpInput = document.getElementById("hp-input");
   const evInput = document.getElementById("ev-input");
@@ -89,15 +132,25 @@ function paperWork() {
   const t1Input = document.getElementById("t1-input");
   const t2Input = document.getElementById("t2-input");
   const rqSubInput = document.getElementById("rq-s-input");
+  const weighInInput = document.getElementById("weigh-in-input");
+  const rcaPass = document.getElementById("rca-pass");
+  const rcaFail = document.getElementById("rca-fail");
 
+  const airAllowInput = document.getElementById("allow-airflow-input");
   const inputAirflow = document.getElementById("input-airflow");
+  const twoFidCheck = document.getElementById("250-check");
   const treeHundCheck = document.getElementById("300-check");
   const treeFidCheck = document.getElementById("350-check");
-  const iaqInput = document.getElementById("iaq-input");
   const flowRCheck = document.getElementById("flow-remed-check");
+  const flowPass = document.getElementById("flow-pass");
+  const flowFail = document.getElementById("flow-fail");
+
+  const iaqAllow = document.getElementById("allow-iaq");
+  const iaqInput = document.getElementById("iaq-input");
+  const iaqPass = document.getElementById("iaq-pass");
+  const iaqFail = document.getElementById("iaq-fail");
 
   const bdInput = document.getElementById("bd-input");
-  const fwdInput = document.getElementById("fan-w-draw");
   const passCheck = document.getElementById("pass-check");
   const failCheck = document.getElementById("fail-check");
 
@@ -106,49 +159,18 @@ function paperWork() {
   const outputBtu = document.getElementById("output-btu");
   const furnaceEff = document.getElementById("furnace-eff");
 
-  const genId = document.getElementById("general-id");
-  const furId = document.getElementById("fur-id");
-  const evapId = document.getElementById("evap-id");
-  const miniSplitId = document.getElementById("mini-split-id");
-  const acId = document.getElementById("ac-id");
-  const packUnitId = document.getElementById("pack-unit-id");
-  const heatPumpId = document.getElementById("heat-pump-id");
-  const ductId = document.getElementById("duct-id");
-  const ductExId = document.getElementById("duct-exempt-id");
-  const ductInfoId = document.getElementById("duct-info-id");
-  const rcaId = document.getElementById("rca-id");
-  const airflowId = document.getElementById("airflow-id");
-  const bdId = document.getElementById("bd-id");
-  const fwdId = document.getElementById("fwd-id");
-  const statusId = document.getElementById("status-id");
-  const qcId = document.getElementById("qc-id");
-  const notesId = document.getElementById("notes-id");
-
   const lockCapsCheck = document.getElementById("lock-caps-check");
   const acSecureCheck = document.getElementById("ac-secure-check");
   const fuseSizeCheck = document.getElementById("fuse-size-check");
   const smokeDetectCheck = document.getElementById("smoke-detect-check");
   const gasLegCheck = document.getElementById("gas-leg-check");
 
+  const plugCheck = document.getElementById("plug-in");
+  const clampCheck = document.getElementById("clamp-meter");
   const hardWiredCheck = document.getElementById("hard-wired");
-
-  const genAnsWrapper = document.getElementById("gen-ans-wrapper");
-  const furAnsWrapper = document.getElementById("fur-ans-wrapper");
-  const evapAnsWrapper = document.getElementById("evap-ans-wrapper");
-  const acAnsWrapper = document.getElementById("ac-ans-wrapper");
-  const hPumpAnsWrapper = document.getElementById("hpump-ans-wrapper");
-  const packUnitAnsWrapper = document.getElementById("pac-unit-ans-wrapper");
-  const miniSplitAnsWrapper = document.getElementById("mini-split-ans-wrapper");
-  const leakAnsWrapper = document.getElementById("leak-ans-wrapper");
-  const ductExWrapper = document.getElementById("duct-exempt-wrapper");
-  const ductInfoWrapper = document.getElementById("duct-info-wrapper");
-  const rcaAnsWrapper = document.getElementById("rca-ans-wrapper");
-  const flowAnsWrapper = document.getElementById("flow-ans-wrapper");
-  const bdAnsWrapper = document.getElementById("bd-ans-wrapper");
-  const fwdAnsWrapper = document.getElementById("fwd-ans-wrapper");
-  const statusAnsWrapper = document.getElementById("stat-ans-wrapper");
-  const qcAnsWrapper = document.getElementById("qc-ans-wrapper");
-  const notesAnsWrapper = document.getElementById("notes-ans-wrapper");
+  const fwdInput = document.getElementById("fan-w-draw");
+  const fwdPass = document.getElementById("fwd-pass");
+  const fwdFail = document.getElementById("fwd-fail");
 
   genAnsWrapper.innerHTML = "";
   evapAnsWrapper.innerHTML = "";
@@ -194,12 +216,18 @@ function paperWork() {
     let dateShow = document.createElement("p");
     let adrShow = document.createElement("p");
     let companyShow = document.createElement("p");
+    let rebateShow = document.createElement("p");
+    let sqftShow = document.createElement("p");
     dateShow.textContent = "Date: " + dateInput.value;
     adrShow.textContent = "Address: " + adrInput.value;
     companyShow.textContent = "Company: " + companyInput.value;
+    rebateShow.textContent = "Rebate Project: " + rebateInput.value;
+    sqftShow.textContent = "SQFT Served: " + sqftInput.value;
     genAnsWrapper.appendChild(dateShow);
     genAnsWrapper.appendChild(adrShow);
     genAnsWrapper.appendChild(companyShow);
+    genAnsWrapper.appendChild(rebateShow);
+    genAnsWrapper.appendChild(sqftShow);
   }
 
   // -----------------------------------------------------------------------
@@ -287,7 +315,6 @@ function paperWork() {
         hPumpAnsWrapper.appendChild(tempShow);
       }
     }
-  
   }
   // ------------------------------------------------------------------------
 
@@ -421,17 +448,18 @@ function paperWork() {
 
   if (leakage.value) {
     ductId.style.display = "block";
-    let leakShow = document.createElement("p");
+    let methodShow = document.createElement("p");
     let leakTarget = document.createElement("p");
-    leakShow.textContent = "Result: " + leakage.value + " CFM";
+    let allowShow = document.createElement("p");
+    let leakShow = document.createElement("p");
+    methodShow.textContent = "Method: " + dltMethodInput.value;
     leakTarget.textContent = "Target: " + dlTargetInput.value + "%";
-    leakAnsWrapper.appendChild(leakShow);
+    allowShow.textContent = "Allowance: " + dltAllowInput.value + " CFM";
+    leakShow.textContent = "Result: " + leakage.value + " CFM";
+    leakAnsWrapper.appendChild(methodShow);
     leakAnsWrapper.appendChild(leakTarget);
-  }
-  if (pbsCheck.checked) {
-    let smokeShow = document.createElement("p");
-    smokeShow.textContent = "Pass by smoke: Yes";
-    leakAnsWrapper.appendChild(smokeShow);
+    leakAnsWrapper.appendChild(allowShow);
+    leakAnsWrapper.appendChild(leakShow);
   }
   if (btwFloorsCheck.checked) {
     let btwShow = document.createElement("p");
@@ -448,13 +476,24 @@ function paperWork() {
     actShow.textContent = "Actual Airflow Method: Yes";
     leakAnsWrapper.appendChild(actShow);
   }
-
-  if (weighInInput.checked) {
-    rcaId.style.display = "block";
-    let weighInShow = document.createElement("p");
-    weighInShow.textContent = "Weigh In Method";
-    rcaAnsWrapper.appendChild(weighInShow);
+  if (pbsCheck.checked) {
+    let smokeShow = document.createElement("p");
+    smokeShow.textContent = "Pass by smoke: Yes";
+    leakAnsWrapper.appendChild(smokeShow);
   }
+  if (pbsCheck.checked === false) {
+    if (dltPass.checked) {
+      let passShow = document.createElement("p");
+      passShow.textContent = "Test Passed";
+      leakAnsWrapper.appendChild(passShow);
+    } else if (dltFail.checked) {
+      let failShow = document.createElement("p");
+      failShow.textContent = "Test Failed";
+      leakAnsWrapper.appendChild(failShow);
+    }
+  }
+
+  // --------------------------------------------------------------------
 
   if (lpInput.value) {
     rcaId.style.display = "block";
@@ -481,34 +520,127 @@ function paperWork() {
     rcaAnsWrapper.appendChild(subcoolShow);
   }
 
+  if (weighInInput.checked) {
+    rcaId.style.display = "block";
+    let weighInShow = document.createElement("p");
+    weighInShow.textContent = "Weigh In Method";
+    rcaAnsWrapper.appendChild(weighInShow);
+  }
+
+  if (weighInInput.checked === false) {
+    if (rcaPass.checked) {
+      let passShow = document.createElement("p");
+      passShow.textContent = "Test Passed";
+      rcaAnsWrapper.appendChild(passShow);
+    } else if (rcaFail.checked) {
+      let failShow = document.createElement("p");
+      failShow.textContent = "Test Failed";
+      rcaAnsWrapper.appendChild(failShow);
+    }
+  }
+
+  // ---------------------------------------------------------------
+
   if (inputAirflow.value) {
     airflowId.style.display = "block";
+    let airAllowShow = document.createElement("p");
     let airMeasShow = document.createElement("p");
+    airAllowShow.textContent = "Allowance: " + airAllowInput.value + " CFM";
     airMeasShow.textContent = "Result: " + inputAirflow.value + " CFM";
+    flowAnsWrapper.appendChild(airAllowShow);
     flowAnsWrapper.appendChild(airMeasShow);
   }
 
-  if (treeHundCheck.checked) {
+  if (twoFidCheck.checked) {
+    let twoFidShow = document.createElement("p");
+    twoFidShow.textContent = "250 CFM Per Ton";
+    flowAnsWrapper.appendChild(twoFidShow);
+  } else if (treeHundCheck.checked) {
     let tresHundShow = document.createElement("p");
-    tresHundShow.textContent = "300 CFM";
+    tresHundShow.textContent = "300 CFM Per Ton";
     flowAnsWrapper.appendChild(tresHundShow);
   } else if (treeFidCheck.checked) {
     let tresHundShow = document.createElement("p");
-    tresHundShow.textContent = "350 CFM";
+    tresHundShow.textContent = "350 CFM Per Ton";
     flowAnsWrapper.appendChild(tresHundShow);
-  }
-
-  if (iaqInput.value) {
-    let iaqShow = document.createElement("p");
-    iaqShow.textContent = "IAQ: " + iaqInput.value;
-    flowAnsWrapper.appendChild(iaqShow);
   }
 
   if (flowRCheck.checked) {
     let remedAirShow = document.createElement("p");
     remedAirShow.textContent = "Airflow Remediation: Yes";
-    fwdAnsWrapper.appendChild(remedAirShow);
+    flowAnsWrapper.appendChild(remedAirShow);
   }
+
+  if (flowRCheck.checked === false) {
+    if (flowPass.checked) {
+      let passShow = document.createElement("p");
+      passShow.textContent = "Test Passed";
+      flowAnsWrapper.appendChild(passShow);
+    } else if (flowFail.checked) {
+      let failShow = document.createElement("p");
+      failShow.textContent = "Test Failed";
+      flowAnsWrapper.appendChild(failShow);
+    }
+  }
+
+  // -----------------------------------------------------------
+
+  if (iaqInput.value) {
+    iaqId.style.display = "block";
+
+    let allowShow = document.createElement("p");
+    let iaqShow = document.createElement("p");
+    allowShow.textContent = "Allowance: " + iaqAllow.value + " CFM";
+    iaqShow.textContent = "Result: " + iaqInput.value + " CFM";
+    if (iaqPass.checked) {
+      let pass = document.createElement("p");
+      pass.textContent = "Test Passed";
+      iaqAnsWrapper.appendChild(pass);
+    } else if (iaqFail.checked) {
+      let fail = document.createElement("p");
+      fail.textContent = "Test Failed";
+      iaqAnsWrapper.appendChild(fail);
+    }
+    iaqAnsWrapper.appendChild(allowShow)
+    iaqAnsWrapper.appendChild(iaqShow);
+  }
+
+  // ------------------------------------------------------
+  if (hardWiredCheck.checked) {
+    notesId.style.display = "block";
+    let hWiredShow = document.createElement("p");
+    hWiredShow.textContent = "Furnace Hard-Wired";
+    notesAnsWrapper.appendChild(hWiredShow);
+  }
+
+  if (fwdInput.value) {
+    fwdId.style.display = "block";
+    let fwdShow = document.createElement("p");
+    fwdShow.textContent = "Result: " + fwdInput.value;
+
+    if (plugCheck.checked) {
+      let plugShow = document.createElement("p");
+      plugShow.textContent = "Watt Meter: Plug-In";
+      fwdAnsWrapper.appendChild(plugShow);
+    } else if (clampCheck.checked) {
+      let clampShow = document.createElement("p");
+      clampShow.textContent = "Watt Meter: Clamp";
+      fwdAnsWrapper.appendChild(clampShow);
+    }
+    fwdAnsWrapper.appendChild(fwdShow);
+  }
+
+  if (fwdPass.checked) {
+    let passShow = document.createElement("p");
+    passShow.textContent = "Test Passed";
+    fwdAnsWrapper.appendChild(passShow);
+  } else if (fwdFail.checked) {
+    let failShow = document.createElement("p");
+    failShow.textContent = "Test Failed";
+    fwdAnsWrapper.appendChild(failShow);
+  }
+
+  // ----------------------------------------------------------------
 
   if (bdInput.value) {
     bdId.style.display = "block";
@@ -516,15 +648,8 @@ function paperWork() {
     bdShow.textContent = "Result: " + bdInput.value;
     bdAnsWrapper.appendChild(bdShow);
   }
-  if (fwdInput.value) {
-    fwdId.style.display = "block";
-    let fwdShow = document.createElement("p");
-    fwdShow.textContent = "Result: " + fwdInput.value;
-    fwdAnsWrapper.appendChild(fwdShow);
-  }
 
-  // ----------------------------------------------------------------
-
+  // --------------------------------------------------------
   if (returnLocInput.value) {
     ductInfoId.style.display = "block";
     let returnLocShow = document.createElement("p");
@@ -606,13 +731,6 @@ function paperWork() {
   }
 
   // ---------------------------------------------------------------------
-
-  if (hardWiredCheck.checked) {
-    notesId.style.display = "block";
-    let hWiredShow = document.createElement("p");
-    hWiredShow.textContent = "Furnace Hard-Wired";
-    notesAnsWrapper.appendChild(hWiredShow);
-  }
 }
 // blankInputs(climateZone, tons, inputBtu, furnaceEff, outputBtu, inputAirflow, leakage, fwD, notes)
 
